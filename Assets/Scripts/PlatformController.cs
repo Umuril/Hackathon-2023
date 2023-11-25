dc1c63cec2b5da149074fb78a97ad4ff307aedd0
Eduard Rubio Cholbi <202501@studenti.unimore.it>
2023-11-25 15:45:19 +0100

    Add moving platform

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    float y_level_limit = 25;
+    float blocking_time = 5;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        transform.position = new Vector3(0, 40, 0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.position.y >= y_level_limit)
+        {
+            transform.position += Vector3.down * Time.deltaTime;
+        } else if (blocking_time > 0){
+            blocking_time -= Time.deltaTime;
+        } else {
+            y_level_limit -= 9;
+            blocking_time = 5;
+        }
+    }
+}
