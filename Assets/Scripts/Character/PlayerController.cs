b390822a2c66b68dde3d1f154000b9d0ee084ea1
Francesco Faenza <dev@francescofaenza.it>
2023-11-25 16:51:25 +0100

    player movement

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using static Models;
+
 
 public class PlayerController : MonoBehaviour
 {
+    CharacterController characterController;
     PlayerInputActions player_input_actions_instance;
     [HideInInspector]
     public Vector2 input_Movement;
     [HideInInspector ]
     public Vector2 input_View;
 
+    Vector3 playerMovement;
+
+
+    public PlayerSettingsModel settings;
+    public bool isTargetMode;
+    
+    [Header("Camera")]
     public Transform cameraTarget;
+    public CameraController cameraController;
+    
 
     public void Awake(){
+        characterController = GetComponent<CharacterController>();
         player_input_actions_instance = new PlayerInputActions();
 
         player_input_actions_instance.Movement.Movement.performed += x => input_Movement = x.ReadValue<Vector2>();
@@ -34,8 +45,24 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Update(){
-        // Debug.Log(input_Movement);
-        // Debug.Log(input_View);
+        Movement();
+    }
+
+    private void Movement(){
+        Debug.Log(cameraController);
+
+        playerMovement = cameraController.transform.forward * (settings.ForwardSpeed * input_Movement.y) * Time.deltaTime;
+        playerMovement += cameraController.transform.right * (settings.ForwardSpeed * input_Movement.x) * Time.deltaTime;
+
+        if (!isTargetMode){
+            var originalRotation = transform.rotation;
+            transform.LookAt(playerMovement + transform.position, Vector3.up);
+            var newRotation = transform.rotation;
+
+            transform.rotation = Quaternion.Lerp(originalRotation, newRotation, settings.CharacterRotationSmoothdamp); 
+        }
+
+        characterController.Move(playerMovement);
     }
 
     # region - Enable/Disable -
