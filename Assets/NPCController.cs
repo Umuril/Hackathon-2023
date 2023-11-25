c29bbec72875fe8e446aedb831e2b94925eb77ef
Martin Menabue <martinmnb@live.it>
2023-11-25 13:22:15 +0100

    Added enemy

---
+++ Assets/NPCController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public bool isRunning = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get player position
+        GameObject player = GameObject.Find("Player");
+        Vector3 playerPosition = player.transform.position;
+        // go towards player
+        Vector3 direction = playerPosition - transform.position;
+        // normalize direction
+        direction.Normalize();
+        // move towards player
+        transform.position += direction * speed * Time.deltaTime;
+
+        // rotate towards player
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
+
+        // change animation to run if moving
+        Animator animator = GetComponent<Animator>();
+        if (direction.magnitude > 0.1f)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+        // check if character is moving and change animation
+        if (direction.magnitude > 0.1f)
+        {
+            isRunning = true;
+        }
+
+
+    }
+}
